7a8b29f69d264c67e4c9694387aa6adf17821837
Max Chesnikov <m.chesnikov@app-smart.ru>
2022-06-28 15:42:36 +0300

    Initial Commit

---
+++ TestChatUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestChatUIKit
+//
+//  Created by Максим Чесников on 28.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
